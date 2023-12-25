940115eef2591e2cb9ddebd94719c02263856944
Jai Gupta <126405080+jaigupta04@users.noreply.github.com>
2023-12-25 21:44:30 +0530

    Test Suite

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,18 @@
-const { getWeekStart, getWeekEnd } = require('../src');
+const { 
+  getWeekStart, 
+  getWeekEnd, 
+  getFy, 
+  getFyMonth, 
+  getFyStart, 
+  getFyEnd, 
+  getQtr, 
+  getTaxQtr, 
+  getFyDuration,
+  tomorrow,
+  today,
+  yesterday,
+  lastWeek
+} = require('../src');
 
 
 
@@ -27,5 +41,67 @@ describe('.getWeekEnd', () => {
   test('2024-01-07', () => expect(getWeekEnd('2024-01-07')).toBe('2024-01-07'));
 
   test('2024-01-31', () => expect(getWeekEnd('2024-01-31')).toBe('2024-02-04')); // Jan → Feb
+
+});
+
+describe('.getFy', () => {
+
+  test('2024-01-01', () => expect(getFy('2024-01-01')).toBe('fy24'));
+  test('2024-04-01', () => expect(getFy('2024-04-01')).toBe('fy25'));
+  test('2024-03-31', () => expect(getFy('2024-03-31')).toBe('fy24'));
+  test('2025-03-31', () => expect(getFy('2025-03-31')).toBe('fy25'));
+
+});
+
+describe('.getFyMonth', () => {
+
+  test('2024-01-01', () => expect(getFyMonth('2024-01-01')).toBe('JAN'));
+  test('2024-03-31', () => expect(getFyMonth('2024-03-31')).toBe('MAR'));
+  test('2025-12-31', () => expect(getFyMonth('2025-12-31')).toBe('DEC'));
+
+});
+
+describe('.getFyStart', () => {
+
+  test('2023-12-31', () => expect(getFyStart('2023-12-31')).toBe('2023-04-01'));
+  test('2024-03-31', () => expect(getFyStart('2024-03-31')).toBe('2023-04-01'));
+  test('2024-04-01', () => expect(getFyStart('2024-04-01')).toBe('2024-04-01'));
+  test('2025-03-31', () => expect(getFyStart('2025-03-31')).toBe('2024-04-01'));
+
+});
+
+describe('.getFyEnd', () => {
+
+  test('2023-12-31', () => expect(getFyEnd('2023-12-31')).toBe('2024-03-31'));
+  test('2024-03-31', () => expect(getFyEnd('2024-03-31')).toBe('2024-03-31'));
+  test('2024-04-01', () => expect(getFyEnd('2024-04-01')).toBe('2025-03-31'));
+  test('2025-03-31', () => expect(getFyEnd('2025-03-31')).toBe('2025-03-31'));
+
+});
+
+describe('.getQtr', () => {
+
+  test('2024-04-01', () => expect(getQtr('2024-04-01')).toBe('q1'));
+  test('2024-07-01', () => expect(getQtr('2024-07-01')).toBe('q2'));
+  test('2024-10-01', () => expect(getQtr('2024-10-01')).toBe('q3'));
+  test('2025-01-01', () => expect(getQtr('2025-01-01')).toBe('q4'));
+
+});
+
+describe('.getTaxQtr', () => {
+
+  test('2024-04-01', () => expect(getTaxQtr('2024-04-01')).toBe('q1'));
+  test('2024-06-16', () => expect(getTaxQtr('2024-06-16')).toBe('q2'));
+  test('2024-09-16', () => expect(getTaxQtr('2024-09-16')).toBe('q3'));
+  test('2025-12-16', () => expect(getTaxQtr('2025-12-16')).toBe('q4'));
+  test('2025-01-16', () => expect(getTaxQtr('2025-01-16')).toBe('q4'));
+  test('2025-03-16', () => expect(getTaxQtr('2025-03-16')).toBe('q5'));
+
+});
+
+describe('.getFyDuration', () => {
+
+  test('2024-04-01', () => expect(getFyDuration('2024-04-01')).toBe(365));
+  test('2023-06-16', () => expect(getFyDuration('2023-06-16')).toBe(366));
 
 });
